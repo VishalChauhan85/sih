70af8c8b73c5fb5d042c903e100ba2e4dd12f6af
Vishal Chauhan <chauhanvishal53381@gmail.com>
2026-05-08 00:29:28 +0530

    setup vercel deployment

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.use((req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => console.log(`Running on http://localhost:${PORT}`));
+}
+
+module.exports = app;
